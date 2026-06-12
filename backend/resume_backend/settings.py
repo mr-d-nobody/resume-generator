144fe7bed8afc71dfd8fa694a89930a2d540918b
mr-d-nobody <mr-d-nobody@users.noreply.github.com>
2026-06-13 05:15:26 +0530

    fix: Add requirements.txt and prepare Django settings for Render

--- backend/resume_backend/settings.py
+++ backend/resume_backend/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h$-fa(t6jk57m1rn+8h*^_+h1=_!)nb%=fqigg7(#77=co&x+p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
